a4289cee99d4da42db9bb47a29fe39f20eb02ac7
plentakill <alan.jan@metech.eu>
2023-02-24 15:47:55 +0100

    Refactored EX4 & EX5

--- src/Ex5.java
+++ src/Ex5.java
@@ -7,10 +7,8 @@ public class Ex5 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a positive integer: ");
         int n = scanner.nextInt();
-        int sum = 0;
         for (int i = 1; i <= n; i++) {
-            sum += i;
+            System.out.println(i * 2);
         }
-        System.out.println("The sum of the first " + n + " natural numbers is " + sum);
     }
 }
